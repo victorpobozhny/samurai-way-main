025aaabd34ee5eacf49da6e828544ec6b993aba6
Victor <pobozhny94@gmail.com>
2023-11-23 19:53:53 +0300

    30. changed main image. UniVerse

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
@@ -1,15 +1,17 @@
 import s from './ProfileInfo.module.css'
 import React from "react";
-import mainImg from "../../../../images/main.jpg";
+import mainImg from "../../../../images/main1.jpg";
+
 
 export function ProfileInfo () {
     return (
-        <>
-            <div className={s.main_image}><img
+        <div className={s.profileInfo}>
+            <div className={s.main_image}>
+                <img
                 src={mainImg}
                 alt={'Main picture'}/>
             </div>
             <div className={s.descriptionBlock}>ava+description</div>
-        </>
+        </div>
     )
 }
